b682e8057a2b28767eb002795a874bb3db6a14c1
josedefrancesco <fra18005@byui.edu>
2026-04-27 22:28:39 -0300

    week_1

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import { getParkData } from "./parkService.mjs";
-import { mediaCardTemplate } from "./templates.mjs";
+import { mediaCardTemplate } from "./templates.mjs"; // Importamos tu nuevo archivo
 
 const parkData = getParkData();
 
-// El instructor pone el array de links aquí o en parkService
+//
 const parkInfoLinks = [
   {
     name: "Current Conditions &#x203A;",
@@ -25,45 +25,47 @@ const parkInfoLinks = [
   }
 ];
 
+//
 function setHeaderInfo(data) {
-  document.querySelector("head > title").textContent = data.fullName;
-  const heroImg = document.querySelector(".hero-banner img");
-  if (heroImg) {
-    heroImg.src = data.images[0].url;
-    heroImg.alt = data.images[0].altText;
-  }
+    document.querySelector("head > title").textContent = data.fullName;
+    const heroImg = document.querySelector(".hero-banner img");
+    if (heroImg) {
+        heroImg.src = data.images[0].url;
+        heroImg.alt = data.images[0].altText;
+    }
 }
 
 function setParkIntro(data) {
-  const introEl = document.querySelector(".intro");
-  if (introEl) {
-    introEl.innerHTML = `<h1>${data.fullName}</h1><p>${data.description}</p>`;
-  }
+    document.querySelector(".intro").innerHTML = `
+        <h1>${data.fullName}</h1>
+        <p>${data.description}</p>
+    `;
 }
 
 function setParkInfoLinks(links) {
-  const infoEl = document.querySelector(".info");
-  const html = links.map(mediaCardTemplate);
-  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+    const infoEl = document.querySelector(".info");
+    //
+    const html = links.map(mediaCardTemplate);
+    infoEl.innerHTML = html.join("");
 }
 
 function setParkFooter(data) {
-  const footerEl = document.querySelector("#park-footer");
-  const mailing = data.addresses.find((addr) => addr.type === "Mailing");
-  const voice = data.contacts.phoneNumbers.find((num) => num.type === "Voice");
+    const footerEl = document.querySelector("#park-footer");
+    const mailing = data.addresses.find((addr) => addr.type === "Mailing");
+    const voice = data.contacts.phoneNumbers.find((num) => num.type === "Voice");
 
-  if (footerEl && mailing && voice) {
     footerEl.innerHTML = `
-      <section class="contact-info">
-        <h3>Contact Info</h3>
-        <h4>Mailing Address</h4>
-        <p>${mailing.line1}<br>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
-        <h4>Phone:</h4>
-        <p>${voice.phoneNumber}</p>
-      </section>`;
-  }
+        <section class="contact-info">
+            <h3>Contact Info</h3>
+            <h4>Mailing Address</h4>
+            <p>${mailing.line1}<br>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
+            <h4>Phone:</h4>
+            <p>${voice.phoneNumber}</p>
+        </section>
+    `;
 }
 
+//
 setHeaderInfo(parkData);
 setParkIntro(parkData);
 setParkInfoLinks(parkInfoLinks);
