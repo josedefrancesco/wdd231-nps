b2bb82296aa54614ea83dbb222a34718cad0d00a
josedefrancesco <fra18005@byui.edu>
2026-04-30 22:27:54 -0300

    week_2

--- src/js/main.js
+++ src/js/main.js
@@ -14,19 +14,25 @@ function parkInfoTemplate(info) {
 
 function init() {
     const disclaimer = document.querySelector(".disclaimer > a");
-    disclaimer.href = parkData.url;
-    disclaimer.innerHTML = parkData.fullName;
+    if (disclaimer) {
+        disclaimer.href = parkData.url;
+        disclaimer.innerHTML = parkData.fullName;
+    }
     document.title = parkData.fullName;
 
-    document.querySelector(".hero-banner img").src = parkData.images[0].url;
+    const heroImg = document.querySelector(".hero-banner img");
+    if (heroImg) {
+        heroImg.src = parkData.images[0].url;
+        heroImg.alt = parkData.images[0].altText;
+    }
     document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(parkData);
 
     document.querySelector(".intro").innerHTML = `<h1>${parkData.fullName}</h1><p>${parkData.description}</p>`;
     
     const infoLinks = [
-        { name: "Conditions", link: "#", image: parkData.images[2].url, description: "Current conditions info." },
-        { name: "Fees", link: "#", image: parkData.images[3].url, description: "Fee information." },
-        { name: "Visitor Centers", link: "#", image: parkData.images[9].url, description: "Visitor center locations." }
+        { name: "Current Conditions &#x203A;", link: "conditions.html", image: parkData.images[2].url, description: "See what conditions to expect in the park before leaving on your trip!" },
+        { name: "Fees and Passes &#x203A;", link: "fees.html", image: parkData.images[3].url, description: "Learn about the fees and passes that are available." },
+        { name: "Visitor Centers &#x203A;", link: "visitor_centers.html", image: parkData.images[9].url, description: "Learn about the visitor centers in the park." }
     ];
     document.querySelector(".info").innerHTML = infoLinks.map(mediaCardTemplate).join("");
 
@@ -37,7 +43,17 @@ function setParkFooter(data) {
     const footerEl = document.querySelector("#park-footer");
     const mailing = data.addresses.find((addr) => addr.type === "Mailing");
     const voice = data.contacts.phoneNumbers.find((num) => num.type === "Voice");
-    footerEl.innerHTML = `<h3>Contact Info</h3><p>${mailing.line1}<br>${mailing.city}, ${mailing.stateCode}</p><p>${voice.phoneNumber}</p>`;
+
+    if (footerEl && mailing && voice) {
+        footerEl.innerHTML = `
+            <div class="contact-info">
+                <h3>Contact Info</h3>
+                <h4>Mailing Address</h4>
+                <p>${mailing.line1}<br>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
+                <h4>Phone</h4>
+                <p>${voice.phoneNumber}</p>
+            </div>`;
+    }
 }
 
 init();
