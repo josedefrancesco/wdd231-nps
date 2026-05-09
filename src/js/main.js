c6a2f9d74b2ab8e9f93a5b9fdb62de6f84b8587e
josedefrancesco <fra18005@byui.edu>
2026-05-09 19:15:25 -0300

    week_3

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,59 @@
 import { getParkData } from "./parkService.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
-async function init() {
-  const parkData = await getParkData();
-
-  document.title = parkData.fullName;
-
-  const disclaimer = document.querySelector(".disclaimer > a");
-  if (disclaimer) {
-    disclaimer.href = parkData.url;
-    disclaimer.innerHTML = parkData.fullName;
-  }
-
-  const heroImg = document.querySelector(".hero-banner img");
-  if (heroImg) {
-    heroImg.src = parkData.images[0].url;
-    heroImg.alt = parkData.images[0].altText;
-  }
-
-  const heroContent = document.querySelector(".hero-banner__content");
-  if (heroContent) {
-    heroContent.innerHTML = parkInfoTemplate(parkData);
-  }
-
-  const intro = document.querySelector(".intro");
-  if (intro) {
-    intro.innerHTML = `
-      <h1>${parkData.fullName}</h1>
-      <p>${parkData.description}</p>
-    `;
-  }
-
-const infoLinks = getParkInfoLinks(parkData);
-
-  const info = document.querySelector(".info");
-  if (info) {
-    info.innerHTML = infoLinks.map(mediaCardTemplate).join("");
-  }
-
-  setParkFooter(parkData);
-}
+const parkData = getParkData();
 
 function parkInfoTemplate(info) {
   return `
     <a href="/" class="hero-banner__title">${info.name}</a>
     <p class="hero-banner__subtitle">
-      <span>${info.designation}</span>
-      <span>${info.states}</span>
-    </p>
-  `;
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
+
+function init() {
+    const disclaimer = document.querySelector(".disclaimer > a");
+    if (disclaimer) {
+        disclaimer.href = parkData.url;
+        disclaimer.innerHTML = parkData.fullName;
+    }
+    document.title = parkData.fullName;
+
+    const heroImg = document.querySelector(".hero-banner img");
+    if (heroImg) {
+        heroImg.src = parkData.images[0].url;
+        heroImg.alt = parkData.images[0].altText;
+    }
+    document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(parkData);
+
+    document.querySelector(".intro").innerHTML = `<h1>${parkData.fullName}</h1><p>${parkData.description}</p>`;
+    
+    const infoLinks = [
+        { name: "Current Conditions &#x203A;", link: "conditions.html", image: parkData.images[2].url, description: "See what conditions to expect in the park before leaving on your trip!" },
+        { name: "Fees and Passes &#x203A;", link: "fees.html", image: parkData.images[3].url, description: "Learn about the fees and passes that are available." },
+        { name: "Visitor Centers &#x203A;", link: "visitor_centers.html", image: parkData.images[9].url, description: "Learn about the visitor centers in the park." }
+    ];
+    document.querySelector(".info").innerHTML = infoLinks.map(mediaCardTemplate).join("");
+
+    setParkFooter(parkData);
 }
 
 function setParkFooter(data) {
-  const footerEl = document.querySelector("#park-footer");
+    const footerEl = document.querySelector("#park-footer");
+    const mailing = data.addresses.find((addr) => addr.type === "Mailing");
+    const voice = data.contacts.phoneNumbers.find((num) => num.type === "Voice");
 
-  const mailing = data.addresses.find((addr) => addr.type === "Mailing");
-  const voice = data.contacts.phoneNumbers.find((num) => num.type === "Voice");
-
-  if (footerEl && mailing && voice) {
-    footerEl.innerHTML = `
-      <div class="contact-info">
-        <h3>Contact Info</h3>
-        <h4>Mailing Address</h4>
-        <p>${mailing.line1}<br>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
-        <h4>Phone</h4>
-        <p>${voice.phoneNumber}</p>
-      </div>
-    `;
-  }
+    if (footerEl && mailing && voice) {
+        footerEl.innerHTML = `
+            <div class="contact-info">
+                <h3>Contact Info</h3>
+                <h4>Mailing Address</h4>
+                <p>${mailing.line1}<br>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
+                <h4>Phone</h4>
+                <p>${voice.phoneNumber}</p>
+            </div>`;
+    }
 }
 
 init();
--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -179,34 +179,8 @@ const park = {
   designation: "National Park"
 };
 
-const baseUrl = "https://developer.nps.gov/api/v1/";
-const apiKey = import.meta.env.VITE_NPS_API_KEY;
-console.log("API KEY:", apiKey);
-
-async function getJson(url) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-Api-Key": apiKey
-    }
-  };
-
-  let data = {};
-
-  const response = await fetch(baseUrl + url, options);
-
-  if (response.ok) {
-    data = await response.json();
-  } else {
-    throw new Error("response not ok");
-  }
-
-  return data;
-}
-
-export async function getParkData() {
-const parkData = await getJson("parks?parkCode=glac");
-  return parkData.data[0];
+export function getParkData() {
+  return park;
 }
 
 export function getParkInfoLinks(data) {
